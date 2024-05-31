969f2ccea9eb60112acdb4eb8e9d518c1f32d8c7
Evelin Melendez Atencio <sandreaeve.rb@gmail.com>
2024-05-31 16:57:32 +0200

    bild: creacion de componentes y nuevas directivas de angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title : string = 'Hola mundo Angular..!!';
+  users : String[]=[ 'Juan', 'Maria' , 'Jose', 'Pedro','Josefina'];
+  visible : boolean= false;
+  setVisible(): void{
+    this.visible= this.visible ? false:true;
+    console.log('Hemos hecho click en setVisible');
+  }
 }
